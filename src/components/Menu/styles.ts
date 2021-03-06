8d64009976ada1790d8be6374563f3e275c654b6
Luiz Otavio <luizomf@gmail.com>
2021-03-06 11:43:24 -0300

    Menu - Parte 2

--- src/components/Menu/styles.ts
+++ src/components/Menu/styles.ts
@@ -1,17 +1,52 @@
 import styled, { css } from 'styled-components';
+import { Title as HeadingStyles } from '../Heading/styles';
 
 export const Wrapper = styled.div`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    background: ${theme.colors.primary};
+    padding: ${theme.spacings.large};
+    display: flex;
+    position: fixed;
+    z-index: 1;
+    width: 100%;
+    max-width: 32rem;
+    height: 100vh;
+    top: 0;
+    left: 0;
+    transition: all 300ms ease-in-out;
+    overflow-y: auto;
+  `}
 `;
 
 export const Nav = styled.nav`
-  ${({ theme }) => css``}
+  margin: auto;
+  width: 100%;
 `;
 
 export const Logo = styled.nav`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    ${HeadingStyles} {
+      display: flex;
+      justify-content: center;
+      margin: 0;
+      margin-bottom: ${theme.spacings.xxlarge};
+
+      img {
+        border: 0.5rem solid ${theme.colors.secondary};
+      }
+    }
+  `}
 `;
 
 export const OpenClose = styled.a`
-  ${({ theme }) => css``}
+  ${({ theme }) => css`
+    position: fixed;
+    top: ${theme.spacings.medium};
+    color: ${theme.colors.white};
+    background: ${theme.colors.primary};
+    z-index: 2;
+    width: 3rem;
+    height: 3rem;
+    left: 26rem;
+  `}
 `;
