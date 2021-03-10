fda366857c4e2bb1974199fddca84efad5889e57
Luiz Otavio <luizomf@gmail.com>
2021-03-09 21:30:13 -0300

    Correções e checagem de testes restantes

--- src/components/PostTags/styles.ts
+++ src/components/PostTags/styles.ts
@@ -2,7 +2,9 @@ import styled, { css } from 'styled-components';
 
 export const Wrapper = styled.p`
   ${({ theme }) => css`
-    margin: ${theme.spacings.medium} 0;
+    margin: ${theme.spacings.medium} auto;
+    max-width: ${theme.sizes.content};
+    padding: 0 ${theme.spacings.large} 0;
 
     span {
       margin: 0 0 0 0.5rem;
--- src/templates/Base/styles.ts
+++ src/templates/Base/styles.ts
@@ -33,7 +33,7 @@ export const ContentContainer = styled.div`
     max-width: 120rem;
     width: 100%;
     margin: 0 auto;
-    margin-bottom: ${theme.spacings.xhuge};
+    margin-bottom: ${theme.spacings.large};
   `}
 `;
 
