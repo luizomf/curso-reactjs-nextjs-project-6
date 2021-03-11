1f157d93548f85fee8caed2da254e5f8232910d2
Luiz Otavio <luizomf@gmail.com>
2021-03-11 06:32:28 -0300

    Revalidate home in 60s

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -48,6 +48,6 @@ export const getStaticProps: GetStaticProps<StrapiPostAndSettings> = async () =>
         ...defaultLoadPostsVariables,
       },
     },
-    revalidate: 24 * 60 * 60,
+    revalidate: 60,
   };
 };
